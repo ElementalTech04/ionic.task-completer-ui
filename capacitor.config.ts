8a64f215bd4caf2058d9863cec5c0264cc9287b0
Frankie Rodriguez <rfrankie@gmail.com>
2022-11-18 00:14:41 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.task.completer.ui',
+  appName: 'ionic.task-completer-ui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
